c802bbb0acb2e523e5fb60b52776328d64e4aed0
Dagem Tsehay <dagemtsehay4@gmail.com>
2023-10-23 21:02:41 +0300

    rel 1

---
+++ _next/static/chunks/app/page-424ebfddcf06fd7a.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{1798:function(e,t,r){Promise.resolve().then(r.bind(r,80))},80:function(e,t,r){"use strict";r.r(t),r.d(t,{DonateBtn:function(){return DonateBtn}});var n=r(7437),o=r(4033);let DonateBtn=()=>{let e=(0,o.useRouter)();return(0,n.jsx)("button",{onClick:()=>e.push("/donate"),className:"bg-green-500 text-white px-10 py-3 rounded font-semibold mt-5 hover:bg-transparent hover:text-green-500 border-2 border-green-500 ",children:"Donate"})}},622:function(e,t,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=r(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),u=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function q(e,t,r){var n,i={},c=null,a=null;for(n in void 0!==r&&(c=""+r),void 0!==t.key&&(c=""+t.key),void 0!==t.ref&&(a=t.ref),t)s.call(t,n)&&!f.hasOwnProperty(n)&&(i[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===i[n]&&(i[n]=t[n]);return{$$typeof:o,type:e,key:c,ref:a,props:i,_owner:u.current}}t.jsx=q,t.jsxs=q},7437:function(e,t,r){"use strict";e.exports=r(622)},4033:function(e,t,r){e.exports=r(94)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=1798)}),_N_E=e.O()}]);
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { HeroComponent } from './_components/hero'
-export default function Home() {
-  return (
-    <main>
-    <HeroComponent/>
-    </main>
-  )
-}
